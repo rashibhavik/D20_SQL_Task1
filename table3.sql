ffdc4ee17c601079af1f662ad9908fb4e99f9c8e
rashibhavik <rashibhavik@gmail.com>
2025-07-03 01:25:19 +0530

    Add files via upload

---
+++ table3.sql
@@ -0,0 +1,34 @@
+create table Products (
+    product_id int primary key,
+    name varchar,
+    category varchar,
+    price decimal,
+    stock int,
+    supplier_name varchar
+)
+
+select * from Products
+
+insert into products (product_id,name,category,price,stock,supplier_name) values(22,'rishi shau','Electronics',200,20,'vivek')
+
+select * from Products
+
+insert into products (product_id,name,category,price,stock,supplier_name) values(25,'sunny rajput','Furniture',130,27,'varun')
+
+select * from Products
+
+insert into products (product_id,name,category,price,stock,supplier_name) values(29,'sissy shahu','Furniture',16,2,'akash')
+
+select * from Products
+
+insert into products (product_id,name,category,price,stock,supplier_name) values(28,'nena mishra','Electronics',106,22,'kaash')
+
+select * from Products
+
+insert into products (product_id,name,category,price,stock,supplier_name) values(30,'riyy shau','Electronics',1000,27,'summy')
+
+select * from Products
+
+insert into products (product_id,name,category,price,stock,supplier_name) values(32,'rashi bhavik','Furniture',110,5,'divas')
+
+select * from Products
