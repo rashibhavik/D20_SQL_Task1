77a03288c671e25ea159e08a79d3f1142445f360
rashibhavik <rashibhavik@gmail.com>
2025-07-04 23:22:57 +0530

    Add files via upload

---
+++ Task1.sql
@@ -0,0 +1,65 @@
+select * from employees
+
+delete  from employees
+
+select * from employees
+
+E:/DA 20/pratices/table1.csv
+
+copy employees from 'E:/DA 20/pratices/table1.csv' DELIMITER ',' csv header
+
+select * from employees
+
+
+select * from student
+
+delete from student
+
+select * from student
+
+E:/DA 20/pratices/table2.csv
+
+copy student from 'E:/DA 20/pratices/table2.csv' DELIMITER ',' csv header
+
+select * from student
+
+select * from products
+
+delete from products
+
+select * from products
+
+E:/DA 20/pratices/table3.csv
+
+copy products from 'E:/DA 20/pratices/table3.csv' DELIMITER ',' csv header
+
+select * from products
+
+select * from orders
+
+delete from orders
+
+select * from orders
+
+E:/DA 20/pratices/table4.csv
+
+copy orders from 'E:/DA 20/pratices/table4.csv' DELIMITER ',' csv header
+
+select * from orders
+
+select * from brand
+
+delete from brand
+
+select * from brand
+
+E:/DA 20/pratices/table5.csv
+
+copy brand from 'E:/DA 20/pratices/table5.csv' DELIMITER ',' csv header
+
+select * from brand
+
+
+
+
+
